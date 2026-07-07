1d2d0bfb82fa3c59ad334f531a9276c43354a8d5
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-07-07 16:14:10 +0100

    add ch-4 exercise

---
+++ c-knr/ch4/ex4-3.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>          /* for atof() */
+#include <ctype.h>
+
+#define MAXOP   100          /* max size of operand or operator */
+#define NUMBER  '0'          /* signal that a number was found */
+#define MAXVAL  100          /* maximum depth of val stack */
+#define BUFSIZE 100
+
+/* functions */
+int    getop(char []);
+void   push(double);
+double pop(void);
+int    getch(void);
+void   ungetch(int);
+
+/* globals */
+int    sp;                   /* next free stack position */
+double val[MAXVAL];          /* value stack */
+char   buf[BUFSIZE];         /* buffer from ungetch */
+int    bufp;                 /* next free position in buf */
+
+int main(){
+	int type;
+	double op2 , op1;
+	char s[MAXOP];
+
+	while ((type = getop(s)) != EOF){
+		switch (type) {
+			case NUMBER:
+				push(atof(s));
+			break;
+			case '+':
+				push(pop() + pop());
+			break;
+			case '*':
+				push(pop() * pop());
+			break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+			break;
+			case '/':
+				op2 = pop();
+				if (op2 != 0.0){
+					push(pop() / op2);
+				}else{
+					printf("error: zero divisor\n");
+				}
+			break;
+			case '%':
+				op2 = pop();
+				op1 = pop();
+				if(op2 != 0.0){
+					push((int)op1 % (int)op2);		
+				}else{
+					printf("error: zero divisor\n");
+				}
+			break;
+			case '\n':
+				printf("\t%.8g\n", pop());
+			break;
+			default:
+				printf("error: unknown command %s\n", s);
+			break;
+			}
+	}
+	return 0;
+}
+
+/* push: push f onto value stack */
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+	int i, c;
+
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+
+	i = 0;
+	if (c == '-')                      /* check sign */
+		if (!isdigit(s[++i] = c = getch())) {
+			ungetch(c);
+			c = s[0];                  /* not a sign */
+		}
+
+	if (!isdigit(c) && c != '.')
+		return c;                      /* not a number */
+
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if( c == '.')                      /* collect fraction part */
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+/* getch: get a (possibly pushed back) character */
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* ungetch: push character back on input */
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+
+
