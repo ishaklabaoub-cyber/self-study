c4b504d0324e5194ba194f20ccee1ebcac4569cb
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-07-08 11:52:37 +0100

    add ch-4 exercise

---
+++ c-knr/ch4/ex4-5.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>          /* for atof() */
+#include <ctype.h>
+#include <math.h>
+
+#define MAXOP   100          /* max size of operand or operator */
+#define NUMBER  '0'   /* signal that a number was found */
+#define WORD    'w'
+#define MAXVAL  100          /* maximum depth of val stack */
+#define BUFSIZE 100
+
+/* functions */
+int    getop(char []);
+void   push(double);
+double pop(void);
+int    getch(void);
+void   ungetch(int);
+
+/* globals */
+int    sp;                   /* next free stack position */
+double val[MAXVAL];          /* value stack */
+char   buf[BUFSIZE];         /* buffer from ungetch */
+int    bufp;                 /* next free position in buf */
+
+int main(){
+	int type;
+	double op2 , op1;
+	char s[MAXOP];
+
+	while ((type = getop(s)) != EOF){
+		switch (type) {
+			case NUMBER:
+				push(atof(s));
+			break;
+			case WORD:
+				op2 = pop();
+				if(!strcmp("sin",s)){
+					push(sin(op2));
+				}else if(!strcmp("exp",s)){
+					push(exp(op2));
+				}else if(!strcmp("pow",s)){
+					op1 = pop();
+					push(pow(op1,op2));
+				}else {
+    					printf("error: unknown function %s\n", s);
+	    				push(op2);  // put the popped value back
+				}
+			break;	
+			case '+':
+				push(pop() + pop());
+			break;
+			case '*':
+				push(pop() * pop());
+			break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+			break;
+			case '/':
+				op2 = pop();
+				if (op2 != 0.0){
+					push(pop() / op2);
+				}else{
+					printf("error: zero divisor\n");
+				}
+			break;
+			case '%':
+				op2 = pop();
+				op1 = pop();
+				if(op2 != 0.0){
+					push((int)op1 % (int)op2);		
+				}else{
+					printf("error: zero divisor\n");
+				}
+			break;
+			case '?':
+				if(sp > 0){
+					printf("\tLast element:%.8g\n",val[sp - 1]);
+				}else{
+					printf("\nStack is Empty.\n");
+				}
+			break;
+			case 'd':
+				if(sp > 0){
+					push(val[sp-1]);
+				}else{
+					printf("\nStack is Empty.\n");
+				}
+			break;
+			case 's':
+				if(sp >= 2){
+					op2 = pop();
+					op1 = pop();
+					push(op2);
+					push(op1);
+				}else{
+					printf("\nYou need atleast two elements.\n");
+				}
+			break;
+			case 'c':
+				sp = 0;	
+			break;
+			case '\n':
+				printf("\t%.8g\n", pop());
+			break;
+			default:
+				printf("error: unknown command %s\n", s);
+			break;
+			}
+	}
+	return 0;
+}
+
+/* push: push f onto value stack */
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+	int i, c;
+
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+
+	i = 0;
+	if (c == '-')                      /* check sign */
+		if (!isdigit(s[++i] = c = getch())) {
+			ungetch(c);
+			c = s[0];                  /* not a sign */
+		}
+	if(isalpha(c)){
+			while(isalpha(s[++i] = c = getch()))
+				;
+		s[i] = '\0';
+		ungetch(c);
+		return WORD;
+	}
+
+	if (!isdigit(c) && c != '.')
+		return c;                      /* not a number */
+
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if(c == '.')                      /* collect fraction part */
+		while (isdigit(s[++i] = c = getch()))
+			;	
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+/* getch: get a (possibly pushed back) character */
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* ungetch: push character back on input */
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+
+
