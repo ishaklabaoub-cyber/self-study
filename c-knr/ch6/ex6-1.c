af06f4782f5f150241901cdf89185bded99251f0
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-08-27 23:59:36 +0100

    ch-6 exercise

---
+++ c-knr/ch6/ex6-1.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFSIZE 100
+//#define NKEYS	(sizeof(keytab) / sizeof(keytab[0]))
+#define MAXWORD 100
+
+char buf[BUFSIZE];
+int  bufp = 0;
+
+struct key{
+	char *word;
+	int count;
+};
+
+int 	getch(void);
+void  	ungetch(int);
+int 	getword(char*,int);
+int 	binsearch(char*,struct key *,int);
+
+
+/*struct key{
+	char *word;
+	int count;
+};*/
+
+struct key keytab[] =
+{
+	{"auto", 0},
+	{"break", 0},
+	{"case", 0},
+	{"char", 0},	
+	{"const", 0},	
+	{"continue", 0},	
+	{"default", 0},
+	/* .... */
+	{"unsigned", 0},	
+	{"void", 0},
+	{"volatile", 0},	
+	{"while", 0},
+};
+#define NKEYS	(sizeof(keytab) / sizeof(keytab[0]))
+
+
+int main()
+{
+	int n;
+	char word[MAXWORD];
+	
+	while(getword(word,MAXWORD) != EOF){
+		if ( isalpha(word[0])){
+				printf("DEBUG : token[%s]\n",word);
+			if((n = binsearch(word, keytab, NKEYS)) >= 0){
+				keytab[n].count++;
+			}
+		}
+	}
+			for(n = 0;n < (int)NKEYS;n++){
+				if(keytab[n].count > 0){
+					printf("%4d %s\n",
+					    keytab[n].count, keytab[n].word);
+				}
+			}
+	putchar('\n');
+	
+	return 0;
+}
+int getch(void){
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+void ungetch(int c){
+	if(bufp >= BUFSIZE){
+		printf("ungetch : too many characters pushed back\n");
+	}else{
+		buf[bufp++] = c;
+	}
+}
+
+/* getword: get next word or character from input */
+int getword(char* word,int limit)
+{
+	int c;
+	char *w = word;
+
+	while(isspace(c = getch()))
+		;
+	if (c == '#'){
+		while((c = getch()) != '\n' && c != EOF)
+			;
+		if (c == EOF){
+			return EOF;
+		}
+	}
+	if(c == '/'){
+		c = getch();
+		if (c == '/'){
+			while((c = getch()) != '\n' && c != EOF)
+				;
+			if(c == EOF)
+				return EOF;
+		} else if (c == '*'){
+
+			char prev = '/';
+			while((prev != '*' || c != '/' ) && c != EOF){
+					prev = c;
+					c = getch();	
+			}
+			if(c == EOF)
+				return EOF;
+		} else{
+			ungetch(c);
+			c = '/';
+		}
+	}
+	if (c != EOF){
+		*w++=c;
+	}	
+	if ( !isalpha(c) && c != '_'){
+		*w = '\0';
+		return c;
+	}
+	for(;limit-- > 0;w++)
+		if( !isalnum(*w = getch()) && *w != '_'){
+			ungetch(*w);
+			break;
+		}
+	*w = '\0';
+	return word[0];
+}
+/* binsearch: find word in tab[0]...tab[n-1] */
+int binsearch(char *word, struct key *tab, int n)
+{
+
+    	int cond;
+	int low,high,mid;
+
+	low = 0;
+	high = n -1;
+
+	  while (low <= high) {
+       		 mid = (low + high) / 2;
+       		 if ((cond = strcmp(word, tab[mid].word)) < 0)
+           		 high = mid - 1;
+        	else if (cond > 0)
+            		low = mid + 1;
+        	else
+            		return mid;
+    	   }
+    	return -1;
+}
