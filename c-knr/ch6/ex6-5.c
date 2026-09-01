b20ca6ca9e8d00dfb9347f46675da1c8a26e8c20
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-01 19:36:06 +0100

    ch-6 exercise

---
+++ c-knr/ch6/ex6-5.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define  HASHSIZE 101
+#define  MAXWORD  100
+#define  BUFSIZE  100
+
+struct nlist{		/* table entry:  */
+	struct nlist *next;		/* next entry of the chain */
+	char *name;			/* defined name */
+	char *defn;			/* replacement text */
+};
+
+static struct nlist *hashtab[HASHSIZE];		/* pointer table */
+char	buf[BUFSIZE];
+int 	bufp = 0;
+char    *prev_name = "none";
+
+unsigned hash(char*);
+struct nlist *lookup(char*);
+struct nlist *install(char*);
+int    getch();
+void   ungetch(int);
+int    getword(char*,int);
+int    undef(char*);
+
+int main()
+{
+	char word[MAXWORD];
+
+	while( (getword(word,MAXWORD)) != EOF){
+		
+	}
+	return 0;
+}
+int getch(){
+	return (bufp > 0) ? buf[bufp--] : getchar();
+}
+int getword(char *word,int limit){
+	int c;
+	char *w = word;
+
+	while(isspace(c = getch()))
+		;
+
+	if(c == '/'){
+		c = getch();
+
+		if(c == '/'){
+			while((c = getch()) != '\n' && c != EOF)
+				;
+			if(c == EOF)
+				return EOF;
+		} else if(c == '*'){
+			char prev = 0;
+			while(!(prev == '*' && c == '/') && c != EOF){
+				prev = c;
+				c = getch();
+			}
+			if(c == EOF)
+				return EOF;
+			c = getch();
+		} else{
+			ungetch(c);
+			c = '/';
+		}
+	}
+	if(c == '"'){
+		int prevc = 0;
+		while((c = getch()) != EOF){
+			if(c == '"' && prevc != '\\')
+				break;
+			prevc = c;
+		}
+		if(c == EOF)
+			return EOF;
+		return getword(word, limit);
+	}
+	if(c != EOF){
+		*w++=c;
+	}
+	if( !isalpha(c) && c != '_'){
+		*w = '\0';
+		return c;
+	}
+	for(;limit-- > 0;w++){
+		if( !isalnum(*w = getch()) && *w != '_'){
+			ungetch(*w);
+			break;
+		}
+	}
+	*w  = '\0';
+	return word[0];
+}
+void ungetch(int c){
+	if(bufp >= BUFSIZE){
+		printf("ungetch : too many characters pushed back\n");
+	} else {
+		buf[bufp++] = c;
+	}
+}
+
+unsigned hash(char *s){
+	unsigned hashval;
+
+	for(hashval = 0; *s != '\0';s++){
+		hashval = *s + 31 * hashval;
+	}
+	return hashval % HASHSIZE;
+}
+
+struct nlist *lookup(char *s){
+	struct nlist *np;
+
+	for((np = hashtab[hash(s)]);np != NULL;np = np->next){
+		if(strcmp(s,np->name) == 0){
+			return np;    /* found */	
+		}
+		prev_name = np->name;
+	}
+	return NULL;	/* not found */
+}
+
+
+struct nlist *install(char *s){
+	unsigned hashval;
+	struct nlist *np;
+	
+	if( (np = lookup(s)) == NULL){	/* not found */
+		np = (struct nlist *)malloc(sizeof(struct nlist));
+		if(np == NULL || (np->name = strdup(s)) == NULL)
+			return NULL;
+		hashval = hash(s);
+		np->next = hashtab[hashval];	
+		hashtab[hashval] = np;
+	} else{		/* already there */
+		free((void *) np->defn);	/* free previous defn */
+		return NULL;
+	}
+	return np;
+}
+
+int undef(char *s){
+	struct nlist *np,*prev;
+	unsigned hashval;
+
+	hashval = hash(s);
+
+	prev = NULL;
+	np = hashtab[hashval];
+
+	while( np != NULL && strcmp(np->name,s) != 0){
+		prev = np;
+		np = np->next;
+	}
+
+		if(np == NULL)
+			return 1;
+		if(prev == NULL){
+			hashtab[hashval] = np->next;
+		} else{
+			prev->next = np->next;
+		}
+			//freeing
+			free((void *)np->name);
+			free((void *)np->defn);
+			free((void *)np);
+
+	return 0;
+}
