5e5074c95583b78e189c72209326570d42d8bf8e
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-08 22:54:59 +0100

    chapter 7 exercise

--- c-knr/ch7/ex7-2.c
+++ c-knr/ch7/ex7-2.c
@@ -18,20 +18,23 @@ int main()
 		if(isprint(c)){
 			putchar(c);
 			column_c++;
-		} else if(/*c == '\t' || 
-			  c == '\a' || 
-			  c == '\n'*/c >= 0 && c <= 32 )
+		} else if(c >= 0 && c <= 32 )
 		{
 			printf("/%#o",c);
-			column_c += 4;	// for example: '/t' --> /011 (Four characters).
-		
+
+			// for column counting
+			if(c >0 && c < 8){
+				column_c += 3;	// because these values has only three characters . ex: '\a' --> /07 
+			} else if(c == '\0'){
+				column_c += 2;	// for the null character '\0' --> /0	
+			} else{
+				column_c += 4;	// for example: '/t' --> /011 (Four characters).
+			}
+
 			if(c == '\n'){	// if encounters with a new line print it as it is
 				putchar('\n');
 				column_c = 0;
 			}
-		} else if(c == '\0'){
-			printf("/%#o",'\0');
-			column_c += 2;
 		}
 	}
 	return 0;
