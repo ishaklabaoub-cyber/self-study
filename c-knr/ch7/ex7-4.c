cd85d1eb0f103714ff1875c5c283982006ed346f
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-11 19:31:43 +0100

    chapter 7 exercise

--- c-knr/ch7/ex7-4.c
+++ c-knr/ch7/ex7-4.c
@@ -1,3 +1,4 @@
+#include <endian.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -10,44 +11,63 @@ int minscanf(char*,...);
 int main()
 {
     int n = 100;
-    
-    //printf("Enter n:");
-    minscanf("enter:%d",&n);
-    printf("n--> %d\n",n);
+    //char c = 'a';
+    //printf("Enter n m:");
+    minscanf("enterd%d", &n);
+    printf("n --> %d\n", n);
 	return 0;
 }
 
 int minscanf(char *fmt,...){
     int c;
     va_list ap; /* points to each argument in turn */
-	int *ival;  
+	int *ival;
+    char *cval;
     char *p;
+
     char temp_str[MAX_LENGTH] = "";
-    int i;
+    int  temp_num;
+    int  flag = 0;
+    int  i,j;
 	
     va_start(ap, fmt);
-	while(isspace(*fmt)){   // Skipping white spaces
-            fmt++;
-    }
 	for(p = fmt; *p; p++){	//scan the format given
+          while(isspace(*p)){
+                p++;
+          }
+
+
+        if(flag){       // if there's a formatting text in fmt , advance p so it points to the '%'
+            --j;
+            p += j;
+        }
         if(*p != '%'){
             i = 0;
-            while(((c = getchar()) != EOF) && c != '\n' && !isdigit(c)){
-                       printf("\t\tDEBUG: wow0\n");
-                       printf("\t\tDEBUG:%c\n",c); 
-                       temp_str[i++] = c;
-                     printf("\t\tDEBUG: temp_str[%s]\n", fmt);
+            j = 0;  
+            while(*(p+j) != '%' && *p != '\0'){   // store the index that '%' is in
+                j++;
             }
-            printf("\t\tDEBUG: wow1\n");
-            temp_str[++i] = '\0';
-            printf("\t\tDEBUG: fmt[%s]\n", fmt);
-            printf("\t\tDEBUG: temp_str[%s]\n", fmt);
-            if(strncmp(temp_str, fmt, i) != 0){
-                printf("\t\tDEBUG: wow2\n");
+            temp_num = 0;
+            while(((c = getchar()) != EOF) && c != '\n' && i < MAX_LENGTH){
+                if(isdigit(c)){
+                    temp_num *= 10;
+                    temp_num += c - '0';
+                } else{
+                    temp_str[i++] = c;      // storing the text given by the user
+                }
+            }
+            temp_str[i] = '\0';
+            
+            int ret;
+            if((ret = strncmp(temp_str, fmt, j)) != 0){     // comparing the text of the user with the formatting in fmt
+                // if they aren't similair assign 0 to the argument passed
                 ival = va_arg(ap, int*);
                 *ival = 0;
-                return 1;
-                break;
+                return 1;  
+            } else if(ret == 0){    
+                // if they are similair we assign later 'temp_num' to the argument passed
+                flag = 1;
+            
             }
                     continue;
         }
@@ -55,8 +75,25 @@ int minscanf(char *fmt,...){
         switch(*p){
             case 'd':
                 ival = va_arg(ap, int*);
+                *ival = 0;
+                if(flag){
+                    flag = 0;
+                    *ival = temp_num;
+                    break;
+                }
                 while(isdigit(c = getchar()) && c != '\n'){
                     *ival = *ival*10 + (c - '0');
+                }
+                break;
+            case 'c':
+                cval = va_arg(ap, char*);
+                    printf("\t\tDEBUG0\n");
+                if( (c = getchar()) == EOF){
+                    printf("\t\tDEBUG1\n");
+                    *cval = 0;
+                } else{
+                    printf("\t\tDEBUG2\n");
+                    *cval = c;
                 }
                 break;
             default:
