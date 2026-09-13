93b5d463850102e1c7fcf7d0d3a6f7d7897457c6
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-13 17:31:06 +0100

    debugging

--- c-knr/ch7/ex7-4.c
+++ c-knr/ch7/ex7-4.c
@@ -1,6 +1,6 @@
-#include <endian.h>
-#include <stdio.h>
+#include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 #include <ctype.h>
 #include <stdarg.h>
 
@@ -11,96 +11,95 @@ int minscanf(char*,...);
 int main()
 {
     int n = 100;
-    //char c = 'a';
-    //printf("Enter n m:");
-    minscanf("enterd%d", &n);
-    printf("n --> %d\n", n);
+    int m = 200;
+    //char temp[MAX_LENGTH] = "hello";
+    minscanf("enter%d %d", &n, &m);
+    printf("n m --> %d %d\n", n, m);
 	return 0;
 }
 
 int minscanf(char *fmt,...){
-    int c;
-    va_list ap; /* points to each argument in turn */
-	int *ival;
+    int c, j;
+    va_list ap;
+    int *ival;
     char *cval;
     char *p;
+    char conversion_type;
+    char value[MAX_LENGTH] = "";
 
-    char temp_str[MAX_LENGTH] = "";
-    int  temp_num;
-    int  flag = 0;
-    int  i,j;
-	
     va_start(ap, fmt);
-	for(p = fmt; *p; p++){	//scan the format given
-          while(isspace(*p)){
-                p++;
-          }
-
-
-        if(flag){       // if there's a formatting text in fmt , advance p so it points to the '%'
-            --j;
-            p += j;
+    for(p = fmt; *p; p++){
+        conversion_type = 0;
+        while(isspace(*p)){     // skipping white spaces
+            p++;
         }
+
         if(*p != '%'){
-            i = 0;
-            j = 0;  
-            while(*(p+j) != '%' && *p != '\0'){   // store the index that '%' is in
+            while((c = getchar()) != EOF && c != '\n' && *p != '%' && *p != '\0'){
+                    if(c != *p){
+                        conversion_type = 'v';
+                        break;
+                    }
+                    p++;
+            }
+        }
+            while(isspace(*p)){
+                p++;
+            }
+        if(*p == '%' && conversion_type != 'v'){
+            j = 0;
+            while(isspace(*p)){
+                p++;
+            }
+            conversion_type = *++p;
+                  
+
+          /*while(isspace(c = getchar()))
+                ;
+            c = 0;*/
+            while((c = getchar()) != EOF && c != '\n' && !isspace(c) && j < MAX_LENGTH){
+                value[j] = c;
                 j++;
             }
-            temp_num = 0;
-            while(((c = getchar()) != EOF) && c != '\n' && i < MAX_LENGTH){
-                if(isdigit(c)){
-                    temp_num *= 10;
-                    temp_num += c - '0';
-                } else{
-                    temp_str[i++] = c;      // storing the text given by the user
-                }
-            }
-            temp_str[i] = '\0';
-            
-            int ret;
-            if((ret = strncmp(temp_str, fmt, j)) != 0){     // comparing the text of the user with the formatting in fmt
-                // if they aren't similair assign 0 to the argument passed
-                ival = va_arg(ap, int*);
-                *ival = 0;
-                return 1;  
-            } else if(ret == 0){    
-                // if they are similair we assign later 'temp_num' to the argument passed
-                flag = 1;
-            
-            }
-                    continue;
+            value[j] = '\0';
+            printf("\t\tDEBUG: value[%s]\n", value);
         }
-        ++p;
-        switch(*p){
+        while(isspace(c = getchar()))
+                ;
+        printf("\t\tDEBUG0\n");
+        j = 0;
+        switch(conversion_type){
+            case 'v': 
+            case '0':
+                // mismatch values not assigned
+                return 1;
+                break;
+            case '%':
+                putchar(conversion_type);
+                break;
             case 'd':
-                ival = va_arg(ap, int*);
+                ival  = va_arg(ap, int*);
                 *ival = 0;
-                if(flag){
-                    flag = 0;
-                    *ival = temp_num;
-                    break;
-                }
-                while(isdigit(c = getchar()) && c != '\n'){
-                    *ival = *ival*10 + (c - '0');
+                while(isdigit(value[j]) && value[j] != '\0'){
+                    *ival = *ival * 10 + (value[j] - '0');
+                    j++;
                 }
                 break;
             case 'c':
-                cval = va_arg(ap, char*);
-                    printf("\t\tDEBUG0\n");
-                if( (c = getchar()) == EOF){
-                    printf("\t\tDEBUG1\n");
-                    *cval = 0;
-                } else{
-                    printf("\t\tDEBUG2\n");
-                    *cval = c;
-                }
+                cval  = va_arg(ap, char*);
+                *cval = value[0];
+                break;
+            case 's':
+                cval  = va_arg(ap, char*);
+                strcpy(cval, value);
                 break;
             default:
-                putchar(*p);
+                putchar(conversion_type);
                 break;
         }
-	}
-    va_end(ap);
+
+
+    
+    }
     return 0;
 }
