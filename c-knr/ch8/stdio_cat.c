9ecefe48e674195fca1a0efdf0d3a9ac4a3e7770
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-17 15:33:25 +0100

    two versions of cat to see the speed diff

---
+++ c-knr/ch8/stdio_cat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void display_files(char *path, int filenbr);
+
+
+int main(int argc, char *argv[])
+{
+    char *lineptr = NULL;
+    size_t n = 0;
+    int filenbr;
+    char *path;
+    if(argc == 1){
+
+        while(getline(&lineptr, &n, stdin) > 0){
+            printf("%s", lineptr);
+        }
+
+    } else if(argc > 1){
+        
+        filenbr = argc - 1;
+        
+        for(int i = 0;i < argc -1; i++){
+            path = argv[1 + i];
+            display_files(path, filenbr);
+        }
+    }
+    return 0;
+}
+
+
+void display_files(char *path, int filenbr){
+    FILE *fp;
+    char *lineptr = NULL;
+    size_t n = 0;
+
+    fp = fopen(path, "r");
+    if(fp == NULL){
+        perror("fopen");
+        return;
+    }
+    if(filenbr > 1){
+        printf("%s\n", path);
+    }
+    while(getline(&lineptr, &n, fp) > 0){
+        printf("%s",lineptr);
+    }
+    fclose(fp);
+}
