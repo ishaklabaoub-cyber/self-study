a7e10acd88f1e5a43841b08be88d72729598d4c7
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-18 11:08:24 +0100

    two versions of cat to see the speed diff

--- c-knr/ch8/sys_cat.c
+++ c-knr/ch8/sys_cat.c
@@ -1,17 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define BUFSIZE 1024
+
+void read_file(char *path, int filenbr);
 
 int main(int argc,char *argv[])
 {
     char buf[BUFSIZE];
-    ssize_t n = 0;
+    int n, filenbr;
+    char *path;
+
     if(argc == 1){
-        while((n = read(0, buf, n))){
-            write(1, buf, n);            
+        while((n = read(0, buf, BUFSIZE)) > 0){
+            if(write(1, buf, n) != n)
+                perror("write");
         }
+    } else if(argc > 1){
+
+        filenbr = argc - 1;
+        
+        for(int i = 0;i < argc - 1;i++){
+            path = argv[i + 1];
+            read_file(path, filenbr);
+        }
+    
     }
 
     return 0;
+}
+
+void read_file(char *path, int filenbr){
+    int  fd, n;
+    char buf[BUFSIZE];
+    
+    fd = open(path, O_RDONLY, 0);
+    if(fd == -1){
+        perror("open");
+        return ;
+    }
+    
+    if(filenbr > 1)
+        printf("%s:\n", path);
+
+    while ((n = read(fd, buf, BUFSIZE)) > 0){
+        if(write(1, buf, n) != n)
+            perror("write");
+    }
 }
